e6a6bf204810c19610bcd0928bb33786d06c3690
testUser <shubh_tiwari@hotmail.com>
2020-06-25 14:14:14 +0530

    React native first project

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Home from './src/screens/Home'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Home: Home
   },
-});
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      title: "First react native project"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
+
+
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
+
---
+++ src/screens/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Text, StyleSheet, View} from "react-native";
+
+
+//Creating variable 
+const text_info = "Hi Shobhakar this side" ;
+
+//Function returning JSX
+const Home = () => {
+    return <View>
+            <Text style={styles.fontSize}>{text_info}</Text>
+         </View>
+};
+
+
+//Styling goes here ----
+const styles = StyleSheet.create({
+    text: {
+      fontSize: 100
+    }
+  });
+
+//export 
+export default Home;
