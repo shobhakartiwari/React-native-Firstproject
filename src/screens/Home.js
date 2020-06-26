615bc3c90b7fe06b6890a127c63733ac1d85b6a7
testUser <shubh_tiwari@hotmail.com>
2020-06-26 14:49:15 +0530

    list attached.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,15 +2,15 @@ import React from "react";
 import {Text, StyleSheet, View} from "react-native";
 
 
-//Creating variable 
-const text_info = "Hi Shobhakar this side" ;
+//constant 
+const txtInfo = ["1,2,3,4,5", "react", "ios", "android"];
 
 //Function returning JSX
 const Home = () => {
-    return (
-    <View>
-            <Text style={styles.fontSize}>{text_info}</Text>
-    </View>
+    return ( 
+        <View>
+            <Text style={styles.fontSize}> {txtInfo}  </Text>
+        </View>
 );
 };
 
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,48 +1,68 @@
 import React from 'react';
-import {Text , View, StyleSheet}  from "react-native";
+import {Text , StyleSheet}  from "react-native";
 import { FlatList } from 'react-native-gesture-handler';
 
 
+
+//Data source 
+const friendsArray = [
+            {name : "Eddie",     key : '1'},
+            {name : "Sahildddd", key : '3'}
+    ];
+
+
 const ListScreen = () => {
-
-    //Array of objects 
-    const friendsArray = [
-        {name : 'shobhakar',key:'1'},
-        {name : 'Krishnendra',key:'2'},
-        {name : 'Vivek',key:'3'},
-        {name : 'Dharmendra',key:'4'},
-        {name : 'Nikhil',key:'5'},
-        {name : 'Manoj',key:'6'},
-        {name : 'Lokesh',key:'7'},
-        {name : 'Himanshu',key:'8'},
-        {name : 'Shikha',key:'9'},
-        {name : 'shobhakar',key:'10'},
-        {name : 'shobhakar',key:'11'},
-        {name : 'shobhakar',key:'12'},
-        {name : 'shobhakar',key:'13'}
-];
-
-
-
     return (
-    <FlatList 
-    data={friendsArray}
-    renderItem = { ({item}) => {
-            //element = {item:{name:shobhakar},index:0}
-            //and then  item : {name:shobhakar}
-
-    return <Text>{item.name}</Text>
-    }}
-    />
-    );
+        <FlatList 
+                    data={friendsArray}
+                    renderItem = { ({item}) => {
+                        //element = {item:{name:shobhakar},index:0}
+                        //and then  item : {name:shobhakar}
+                        return <Text > {item.name} </Text>;
+                }}
+            />
+        );
 };
 
 
-const styles = StyleSheet.create(
-    {
-        
-    }
-);
+
+// const ListScreen = () => {
+
+//     //Array of objects 
+//     const friendsArray = [
+//         {name : 'shobhakar',key:'1'},
+//         {name : 'Krishnendra',key:'2'},
+//         {name : 'Vivek',key:'3'},
+//         {name : 'Dharmendra',key:'4'},
+//         {name : 'Nikhil',key:'5'},
+//         {name : 'Manoj',key:'6'},
+//         {name : 'Lokesh',key:'7'},
+//         {name : 'Himanshu',key:'8'},
+//         {name : 'Shikha',key:'9'},
+//         {name : 'shobhakar',key:'10'},
+//         {name : 'shobhakar',key:'11'},
+//         {name : 'shobhakar',key:'12'},
+//         {name : 'shobhakar',key:'13'}
+// ];
+
+    // return (
+    // <FlatList 
+    //         data={friendsArray}
+    //             renderItem = { ({item}) => {
+    //                 //element = {item:{name:shobhakar},index:0}
+    //                 //and then  item : {name:shobhakar}
+    //                 return <Text styles={styles.textStyle}>{item.name}</Text>;
+    //         }}
+    //     />
+    // );
+// };
+
+
+// const styles = StyleSheet.create({
+//         textStyle:{
+//             marginVertical:50
+//         }
+//     });
 
 
 export default ListScreen ;
